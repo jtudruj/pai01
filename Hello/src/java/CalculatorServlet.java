4f6ec15782aea195bc274bc54ec1f0986879c392
jtudruj <jakub.tudruj@pollub.edu.pl>
2016-12-11 01:10:58 +0100

    sesja

--- Hello/src/java/CalculatorServlet.java
+++ Hello/src/java/CalculatorServlet.java
@@ -4,12 +4,18 @@
  * and open the template in the editor.
  */
 
+import com.sun.xml.internal.ws.util.StringUtils;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  *
@@ -17,6 +23,16 @@ import javax.servlet.http.HttpServletResponse;
  */
 public class CalculatorServlet extends HttpServlet {
 
+    HttpSession session;
+    String lastOperation;
+    
+    @Override
+    public void init() throws ServletException {
+        super.init(); //To change body of generated methods, choose Tools | Templates.
+        this.lastOperation = "";
+    }
+
+    
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
      * methods.
@@ -38,10 +54,43 @@ public class CalculatorServlet extends HttpServlet {
             out.println("</head>");
             out.println("<body>");
             
-            out.println("<h1>Wynik operacji</h1>");
+            this.session = request.getSession();
             
-            out.println("<p>" + this.calculationResult(request) + "</p>");            
-
+            if (this.isClearSessionRequest(request)) {
+                if (this.session != null) {
+                    this.session.removeAttribute("lastOperations");
+                    out.println("<p>Sesja wyczyszczona! <a href=\"http://localhost:8080/Hello/Calculator.html\">Wroc do kalkulatora</a></p>");
+                }
+            }
+            
+            String result = "<p>" + this.calculationResult(request) + "</p>";
+            if (this.isFormValid(request)) {
+                out.println("<h1>Wynik operacji</h1>");
+                
+                out.println(result); 
+            } else {
+                out.println("<p>Invalid intput!!!</p>");
+            }
+            out.println("<h1>Historia operacji</h1>");
+                
+            this.lastOperation = result + (String)this.session.getAttribute("lastOperations");
+            if (lastOperation == null) {
+                out.println("<p>Brak historii</p>");
+            } else {
+                out.println(lastOperation);                
+            }
+            if (this.isFormValid(request)) {
+                lastOperation += result; 
+                this.session.setAttribute("lastOperations", lastOperation);
+            }
+                
+            out.println("<p><a href=\"http://localhost:8080/Hello/Calculator.html\">Nowa operacja</a></p>");
+            out.println(
+                "<form action=\"CalculatorServlet\" method=\"post\">\n" +
+                "  <button type=\"submit\" name=\"clearSession\" value=\"clearSession\">Wyczysc sesje</button>\n" +
+                "</form>"
+            );
+            
             //out.println("<h1>Servlet CalculatorServlet at " + request.getContextPath() + "</h1>");
             out.println("</body>");
             out.println("</html>");
@@ -71,6 +120,30 @@ public class CalculatorServlet extends HttpServlet {
             default:
                  return 0;
         }
+    }
+    
+    private boolean isClearSessionRequest(HttpServletRequest request) {
+        String param = request.getParameter("clearSession");
+        return param != null;
+    }
+    
+    private boolean isFormValid(HttpServletRequest request) {
+        return !request.getParameter("firstNumber").equals("") &&
+                !request.getParameter("secondNumber").equals("") &&
+                request.getParameter("firstNumber") != null &&
+                request.getParameter("secondNumber") != null &&
+                this.isNumeric(request.getParameter("firstNumber")) &&
+                this.isNumeric(request.getParameter("secondNumber"));
+    }
+    
+    private boolean isNumeric(String s) {  
+        return s.matches("[-+]?\\d*\\.?\\d+");  
+    } 
+    
+    public boolean isValid(String input, String reg) {
+        Pattern pattern = Pattern.compile(reg);
+        Matcher seq = pattern.matcher(input);
+        return seq.find();
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
